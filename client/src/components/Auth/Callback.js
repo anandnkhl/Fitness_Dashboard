125a0073ef09b1c8238cc2de5d60686a60402830
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-07-23 04:43:24 +0530

    added callback

---
+++ client/src/components/Auth/Callback.js
@@ -0,0 +1,14 @@
+import React from "react";
+import loading from "./loading.svg";
+import "../../styles/App.css";
+
+const Callback = () => {
+
+  return (
+    <div className="logo">
+      <img src={loading} alt="loading"/>
+    </div>
+  );
+};
+
+export default Callback;
