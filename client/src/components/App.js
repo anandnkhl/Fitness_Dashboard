9bcfe07a90dfa56d2d1fe47e1e4d8b03e2a84afd
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-07-23 04:46:08 +0530

    created basic dashboard for normalUser and admin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import './App.css';
-import { useAuth0 } from "@auth0/auth0-react";
+import jwt_decode from 'jwt-decode'
+import Header from './Header';
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+import { useAuth0 } from "./Auth/react-auth0-spa";
+import MyInfoQuery from "./MyInfoQuery";
 
-function App() {
-  const { loginWithRedirect } = useAuth0();
-  return (
-    <div className="App">
-      <header className="App-header">
-        Login:
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      </header>
-    </div>
-  );
+
+let Role = 'normalUser';
+const createApolloClient = (authToken) => {
+	return new ApolloClient({
+		link: new HttpLink({
+		uri: 'https://relaxed-pig-38.hasura.app/v1/graphql',
+		headers: {
+			Authorization: `Bearer ${authToken}`,
+			'X-Hasura-Role': `${Role}`
+		}
+		}),
+		cache: new InMemoryCache(),
+	});
+};
+
+function App({ idToken }) {
+	const { loading,  logout } = useAuth0();
+	
+	const decoded = jwt_decode(idToken);
+	if(decoded.email === 'anandnikhil91@gmail.com'){
+		Role = 'admin';
+	}
+
+
+	if (loading || !idToken) {
+		return <div>Loading...</div>;
+	}
+	const client = createApolloClient(idToken);
+	return (
+		<ApolloProvider client={client}>
+		<div className = "overlay-message">
+			<Header logoutHandler={logout} />
+			<MyInfoQuery />
+		</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
